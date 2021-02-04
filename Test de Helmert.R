0c7fdab709a543c61af1fc4c4461771b46beb515
Angel Saucedo <1340048h@umich.mx>
2021-02-04 11:15:02 -0600

    Commit de prueba

---
+++ Test de Helmert.R
@@ -0,0 +1,6 @@
+# Código para realizar la validación de datos de precipitaciones
+# Se desarrollan el test de Helmert, prueba de secuencias y limites de Anderson
+
+# Paso 1: Se importarán y se les dará la estructura necesaria a los datos para
+# ser tratados
+
